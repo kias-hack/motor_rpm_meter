6b4bfb9820ca73177e52dcec485332d98bd206cb
alexander <al.kiri4enko2015@yandex.ru>
2025-11-12 23:34:20 +0300

    обновил прошивку и схему, теперь это устройство должно внедряться в корпус стробоскопа и брать сигнал из стробоскопа

--- rpm_meter_and_voltmeter.ino
+++ rpm_meter_and_voltmeter.ino
@@ -1,97 +1,90 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
 
-#define IDLING_RPM 800
-#define ONE_MINUTE_IN_SECONDS 60
-#define TIM1_OCR_VALUE (8000000 / 1024) / ((IDLING_RPM / ONE_MINUTE_IN_SECONDS) * 2)
-#define TEST_IDLING_OUTPUT 4
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
 
-#define VOLTMETER_PIN 1
-#define VOLTMETER_RESISTORS_COEFF (9910 / 728) * (1.22 / 128)
-#define VOLTMETER_INTERVAL_MS 500
+ This example is for a 128x32 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
 
-#define FREQ_PIN 0
-#define DISPLAY_INTERVAL_MS 100
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
 
-long voltmeter_interval = 0;
-long display_interval = 0;
-volatile long rpm = 0;
-float voltage = 0;
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
 
-LiquidCrystal_I2C LCD(0x27,16,2);
-
-volatile int counter = 0;
-volatile long prev = 0;
-
-void freq_handler() {
-  if(counter == 0) {
-    prev = micros();
-    counter = 1;
-  } else if(counter == 1){
-    rpm = (1000000 * ONE_MINUTE_IN_SECONDS/2) / (micros() -  prev);
-    counter = 0;
-  }
-}
-
-ISR(TIMER1_COMPA_vect)
-{
-    digitalWrite(TEST_IDLING_OUTPUT, !digitalRead(TEST_IDLING_OUTPUT));
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println(TIM1_OCR_VALUE);
-
-  LCD.init();
-  LCD.backlight();
-
-  pinMode(VOLTMETER_PIN, INPUT);
-  pinMode(FREQ_PIN, INPUT);
-  pinMode(TEST_IDLING_OUTPUT, OUTPUT);
-
-  attachInterrupt(FREQ_PIN, freq_handler, RISING);
-
-  cli();
-
-  TCCR1A = 0;   // установить регистры в 0
-  TCCR1B = 0;
-
-  TCCR1B |= (1 << WGM12);  // включить CTC режим 
-  TCCR1B |= (1 << CS10); // Установить биты на коэффициент деления 1024
-  TCCR1B |= (1 << CS12);
-
-  TIMSK1 |= (1 << OCIE1A); // прерывание по совпадению
-  OCR1A = TIM1_OCR_VALUE;
-
-  sei();
-
-  analogReference(INTERNAL);
-}
-
-void loop() {
-  if(micros() -  prev > 1000000) {
-    rpm = 0;
-  }
-
-  if(millis() - voltmeter_interval >= VOLTMETER_INTERVAL_MS) {
-    voltmeter_interval = millis();
-
-    voltage = (float)(analogRead(VOLTMETER_PIN) >> 3) * VOLTMETER_RESISTORS_COEFF;
-  }
-
-  if(millis() - display_interval >= DISPLAY_INTERVAL_MS) {
-    display_interval = millis();
-
-    LCD.setCursor(0, 0); // первая строка
-    LCD.print("rpm: "); 
-    LCD.print(rpm); 
-    LCD.print("                ");
-
-    LCD.setCursor(0, 1); // вторая строка
-    LCD.print("volt: "); 
-    LCD.print(voltage);
-    LCD.print("                ");
-  }
-}
+ #include <SPI.h>
+ #include <Wire.h>
+ #include <Adafruit_GFX.h>
+ #include <Arduino.h>
+ #include <stdlib.h>
+ #include <Adafruit_SSD1306.h>
+ 
+ #define SCREEN_WIDTH 128 // OLED display width, in pixels
+ #define SCREEN_HEIGHT 32 // OLED display height, in pixels
+ 
+ #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+ #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+ unsigned long last_tick;
+ int freq = 100;
+ char freq_str[10];
+ 
+ void print_rpm(const char[]);
+ void freqmeter();
+ 
+ void setup() {
+   attachInterrupt(digitalPinToInterrupt(2), freqmeter, RISING);
+ 
+   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+     for(;;);
+   }
+ 
+   display.display();
+ 
+   delay(1000);
+ }
+ 
+ void print_rpm(const char* rpm) {
+   display.clearDisplay();
+ 
+   display.setTextSize(5);
+   display.setTextColor(SSD1306_WHITE);
+   display.setCursor(0, 0);
+   display.cp437(true);
+ 
+   display.print(rpm);
+   
+   display.display();
+ }
+ 
+ void freqmeter() {
+   if (last_tick > 0) {
+     unsigned long diff = micros() - last_tick;
+ 
+     freq = 30000000 / diff; // импульсы/сек / 2 (импульсов катушки за один оборот распредвала, но мы считаем обороты коленвала, поэтому делим на 2) * 60 (секунд)
+   }
+ 
+   last_tick = micros();
+ }
+ 
+ void loop() {
+   itoa(freq, freq_str, 10);
+   print_rpm(freq_str);
+ 
+   if (last_tick > 0) {
+     unsigned long diff  = micros() - last_tick;
+     if (diff > 500000) {
+       freq = 0;
+       last_tick = 0;
+     }
+   }
+ 
+   delay(40);
+ }
